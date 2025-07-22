e1943b93a7435275fb8c7a457f66e8e2ec695651
이춘호 <lch92458@gmail.com>
2025-07-22 15:34:58 +0900

    yml설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ S3 호스팅 시 상대 경로로 설정
+  build: {
+    outDir: 'dist',
+  },
 })
